20e8bac35eecb11dd43a99258cfafe19ecee4d77
John Henry <john@iamjohnhenry.com>
2019-03-03 09:30:39 -0800

    restore demo

--- demo/index.js
+++ demo/index.js
@@ -14,21 +14,21 @@ tape('Demo', async ({
         plan
     }) => {
     try {
-        // {
-        //     await alert('This is a test');
-        // }
-        // {
-        //     await confirm('Are you ready to start the test?');
-        // }
-        // {
-        //     const answer = await choosedropdown('Choose your unit',
-        //         '0: Introduction',
-        //         '1: After The Beginning...');
-        // }
-        // {
-        //     const answer = await prompt('What is the answer to the ultimate question?', '0');
-        //     equal('42', answer.trim().toLowerCase());
-        // }
+        {
+            await alert('This is a test');
+        }
+        {
+            await confirm('Are you ready to start the test?');
+        }
+        {
+            const answer = await choosedropdown('Choose your unit',
+                '0: Introduction',
+                '1: After The Beginning...');
+        }
+        {
+            const answer = await prompt('What is the answer to the ultimate question?', '0');
+            equal('42', answer.trim().toLowerCase());
+        }
         {
             const answer = await choose('What is the first letter of the english alphabet?', 'A', 'B', 'C');
             equal('A', answer);
