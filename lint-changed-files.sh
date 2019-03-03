023bb09861e9a34066c00762c222198f0605f4ac
John Henry <john@iamjohnhenry.com>
2019-03-03 01:21:35 -0800

    update

--- lint-changed-files.sh
+++ lint-changed-files.sh
@@ -1,8 +1,8 @@
-EEE=$(git diff  --staged --name-only HEAD | grep -E '\.ts$')
+FILES=$(git diff  --staged --name-only HEAD | grep -E '\.ts$')
 
-if [ -z "$EEE" ]
+if [ -z "$FILES" ]
 then
       exit 0
 else
-      npm run tslint --fix $EEE
+      npm run tslint --fix $FILES
 fi
