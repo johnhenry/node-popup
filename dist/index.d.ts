a581e476f992f2cff03640e916a4a49d948e6c6b
John Henry <john@iamjohnhenry.com>
2019-03-03 23:01:22 -0800

    Revert "update typings"
    This reverts commit 772ab3b63b65e54672b2461715788ad09c8a18aa.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,17 @@
+declare module 'src/customized' {
+     const customized: ({ pageBody, style, top, left, width, height, title }?: any) => (message: string | number, ...rest: (string | number)[]) => Promise<{}>;
+    export { customized };
+
+}
 declare module 'src/index' {
-    export const customized: ({ pageBody, style, top, left, width, height, title }?: any) => (message: string | number, ...rest: (string | number)[]) => Promise<{}>;
+    import { customized } from 'src/customized.ts';
     export const alert: (message: string) => Promise<string>;
     export const confirm: (message: string) => Promise<string>;
     export const prompt: (message: string | number, defaultAnswer: string | number) => Promise<string>;
     export const choose: (message: string | number, ...choices: (string | number)[]) => Promise<string>;
     export const choosedropdown: (message: string | number, ...choices: (string | number)[]) => Promise<string>;
     export const choosemultiple: (message: string | number, ...choices: (string | number)[]) => Promise<string[]>;
+    export { customized };
 
 }
 declare module 'src/pages' {
